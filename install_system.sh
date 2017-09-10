db53f89977b96e192784f80cf32f3190bc2752ee
MiyamotoAkira <yefo.akira@gmail.com>
2017-09-10 20:24:43 +0100

    Initial set of commands
    I have added the initial set of commands to be able to setup a Manjaro
    system all ready for development.

---
+++ install_system.sh
@@ -0,0 +1,28 @@
+#general pacman
+pacman -Syu
+
+# git configuration
+pacman -S git
+cp config_files/gitconfig ~/.gitconfig 
+cp config_files/gitignoreglobal ~/.gitignoreglobal
+
+# ZSH configuration
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cshs -s `which zsh`
+cp config_files/akira.zsh-theme  ~/.oh-my-zsh/themes/
+cp config_files/zshrc ~/.zshrc
+
+# rbenv configuration
+
+# lein configuration
+mkdir ~/bin
+cp ~/code/nix_setup/scripts/lein  ~/bin
+chmod a+x ~/bin/lein
+lein
+
+cp ~/code/nix_setup/config_files/lein_profiles.clj ~/.lein/profiles.clj -f
+
+# python pip configuration
+sudo python ~/code/nix_setup/scripts/get-pip.py
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
