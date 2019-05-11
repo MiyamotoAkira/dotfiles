7e9332e0487160af6d6325f88c53ed2c67a4e3f9
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 14:09:29 +0100

    Fixed test

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 ## We will be testing for the version of all apps installed here
 
-if [[ `~/.rbenv/bin/rbenv version` != 'ruby 2.5.1p57 (2018-03-29 revision 63029) [x86_64-linux-gnu]' ]]; then
+if [[ `~/.rbenv/bin/rbenv version` != '2.5.1 (set by /root/.rbenv/version)' ]]; then
     echo `~/.rbenv/bin/rbenv version`
     echo "Wrong ruby version!" 1>&2
     exit 64
