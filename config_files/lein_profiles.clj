9f2549d52ec0b536e2b641f5e1f71ec198146030
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 10:38:14 +0100

    Updated config files

--- config_files/lein_profiles.clj
+++ config_files/lein_profiles.clj
@@ -1,7 +1,15 @@
-{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.20.0"]
-                  [venantius/ultra "0.5.1"]
-                  [lein-pprint "1.1.2"]
-                  [lein-cprint "1.2.0"]
-                  [lein-cljfmt "0.5.6"]
-                  [mvxcvi/whidbey "1.3.1"]]}}
-{:repl {:plugins [[cider/cider-nrepl "0.14.0"]]}}
+{:user
+ {;; :dependencies [[nrepl "0.5.3"]]
+  :plugins [[lein-pprint "1.2.0"]
+            [lein-cprint "1.3.0"]
+            [com.jakemccrary/lein-test-refresh "0.23.0"]
+            [lein-cljfmt "0.6.3"]]}
+ ;; {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]
+ ;;            [venantius/ultra "0.5.2"]
+ ;;            [lein-pprint "1.2.0"]
+ ;;            [lein-cprint "1.3.0"]
+ ;;            [lein-cljfmt "0.6.3"]
+ ;;            [mvxcvi/whidbey "1.3.2"]
+ ;;            [nrepl/lein-nrepl "0.3.1"]]}
+ }
+{:repl {:plugins [[cider/cider-nrepl "0.18.0"]]}}
