85ccc00c9144d082cddcc924e1848b006c942968
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 13:17:32 +0100

    Added the ruby build plugin to install ruby versions

--- common/common-configuration.sh
+++ common/common-configuration.sh
@@ -25,5 +25,7 @@ rm -f "$RUSTUP_TEMP"
 
 # Rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
 ~/.rbenv/bin/rbenv install 2.5.1
