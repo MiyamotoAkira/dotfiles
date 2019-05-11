043a8b87fcd20665288d8a1dc21f2fc323cb5da9
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 22:30:06 +0100

    Adding Chrome

--- fedora/up.sh
+++ fedora/up.sh
@@ -11,8 +11,11 @@ sudo dnf -y groupupdate core
 sudo dnf -y upgrade --best --allowerasing
 sudo dnf -y --best --allowerasing install zsh
 
-sudo dnf -y --best --allowerasing install curl wget util-linux-user mercurial kdiff3 openssl-devel readline-devel ffmpeg-libs dnf-plugins-core 
+sudo dnf -y --best --allowerasing install curl wget util-linux-user mercurial kdiff3 openssl-devel readline-devel ffmpeg-libs dnf-plugins-core  fedora
 
+# Install chrome
+sudo dnf install fedora-workstation-repositories
+sudo dnf config-manager --set-enabled google-chrome
 
 # Dotnet packages
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
--- mint/up.sh
+++ mint/up.sh
@@ -6,8 +6,13 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 # Basic packages
-sudo apt-get -y install zsh curl wget net-tools software-properties-common coreutils xclip libssl-dev libreadline-dev zlib1g-dev mercurial kdiff3
+sudo apt-get -y install zsh curl wget net-tools software-properties-common coreutils xclip libssl-dev libreadline-dev zlib1g-dev mercurial kdiff3 firefox
 
+# Install chrome
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/chrome.list
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
 
 # Dot net packages
 wget -q https://packages.microsoft.com/config/ubuntu/18.10/packages-microsoft-prod.deb
