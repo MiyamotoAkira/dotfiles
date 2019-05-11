6a8bc7a288bbd22bff3bbabcdb812a5728bc5c57
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 12:40:46 +0100

    Set up the tmp file as variable

--- common/common-configuration.sh
+++ common/common-configuration.sh
@@ -8,10 +8,11 @@ mkdir -p ~/code/personal/
 mkdir -p ~/code/codurance/
 
 # Rust
-curl https://sh.rustup.rs -sSf > temp_rust.sh
-chmod +x temp_rust.sh
-temp_rust.sh -y
-rm -f temp_rust.sh
+RUSTUP_TEMP="/temp/temp_rust.sh"
+curl https://sh.rustup.rs -sSf > "$RUSTUP_TEMP"
+chmod +x "$RUSTUP_TEMP"
+"$RUSTUP_TEMP" -y
+rm -f "$RUSTUP_TEMP"
 
 # Rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
