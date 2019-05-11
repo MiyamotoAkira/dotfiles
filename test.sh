3781c2e3ddce1ef033a525ca1834eca3735500aa
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 10:39:13 +0100

    Adding first test

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 #! /bin/zsh
 
 ## We will be testing for the version of all apps installed here
+
+if [[ `ruby --version` != 'ruby 2.5.1p57 (2018-03-29 revision 63029) [x86_64-linux-gnu]' ]]; then
+    echo "Wrong ruby version!" 1>&2
+    exit 64
+fi
