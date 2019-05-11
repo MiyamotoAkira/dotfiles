5ce88a4f56a2135ff5cc3624ec6a0ac6bfdff3d0
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 14:05:01 +0100

    Changed the ruby test for now

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 ## We will be testing for the version of all apps installed here
 
-if [[ `ruby --version` != 'ruby 2.5.1p57 (2018-03-29 revision 63029) [x86_64-linux-gnu]' ]]; then
-    echo `ruby --version`
+if [[ `~/.rbenv/bin/rbenv version` != 'ruby 2.5.1p57 (2018-03-29 revision 63029) [x86_64-linux-gnu]' ]]; then
+    echo `~/.rbenv/bin/rbenv version`
     echo "Wrong ruby version!" 1>&2
     exit 64
 fi
