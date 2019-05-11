42c3ae70fd2baf51a86430dedec3adfd84ea7588
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 17:06:17 +0100

    Adding dotnet to mint installation

--- mint/up.sh
+++ mint/up.sh
@@ -9,4 +9,12 @@ sudo apt-get -y upgrade
 sudo apt-get -y install zsh curl wget net-tools software-properties-common coreutils xclip libssl-dev libreadline-dev zlib1g-dev mercurial kdiff3
 
 
+# Dot net packages
+wget -q https://packages.microsoft.com/config/ubuntu/18.10/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.2
+
 "common/common-configuration.sh"
