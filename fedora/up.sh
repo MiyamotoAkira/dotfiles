2902b31329ee4f30d7a854331e13ae8977e99582
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 03:58:22 +0100

    Added development tools for fedora

--- fedora/up.sh
+++ fedora/up.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+sudo dnf -y groupinstall "C Development Tools and Libraries"
+sudo dnf -y groupinstall "Development Tools"
+
 sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf -y groupupdate core
