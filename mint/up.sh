98d1bb13b35477dcd6e15956fb9b4e23241e191c
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 13:00:22 +0100

    Added needed packages for rbenv to run

--- mint/up.sh
+++ mint/up.sh
@@ -6,9 +6,7 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 # Basic packages
-sudo apt-get -y install zsh curl wget net-tools software-properties-common coreutils xclip
-
-
+sudo apt-get -y install zsh curl wget net-tools software-properties-common coreutils xclip libssl-dev libreadline-dev
 
 
 "common/common-configuration.sh"
