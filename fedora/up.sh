3cbe23d2fbb8fbff73a8a1e6ef2985065ae73fb7
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 03:51:33 +0100

    Added rpm fusion

--- fedora/up.sh
+++ fedora/up.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf -y groupupdate core
 sudo dnf -y upgrade --best --allowerasing
