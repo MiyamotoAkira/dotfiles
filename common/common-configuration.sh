2505e94c23c398a951da0cf03d5b4b2cc5e991a6
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 12:35:44 +0100

    Rust with default

--- common/common-configuration.sh
+++ common/common-configuration.sh
@@ -8,7 +8,10 @@ mkdir -p ~/code/personal/
 mkdir -p ~/code/codurance/
 
 # Rust
-curl https://sh.rustup.rs -sSf | sh
+curl https://sh.rustup.rs -sSf > temp_rust.sh
+chmod +x temp_rust.sh
+temp_rust.sh -y
+rm -f temp_rust.sh
 
 # Rbenv
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
