aeeed3fa8e20bc0a01676139a63e6159a78ed703
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-11 10:38:46 +0100

    Further addition of packages and configuration

--- mint/up.sh
+++ mint/up.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install zsh
 
-chsh -s $(which zsh)
+# Basic packages
+sudo apt-get -y install zsh curl wget net-tools software-properties-common coreutils xclip
+
+
+
+
+"../common/common-configuration.sh"
