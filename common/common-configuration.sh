892d36159ae08315db9c8e4ba05033c6d3e4b413
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-17 17:07:42 +0100

    Trying to fix issue with the configuration of emacs build

--- common/common-configuration.sh
+++ common/common-configuration.sh
@@ -61,6 +61,8 @@ mkdir -p "$EMACS_TEMP_FOLDER"
 EMACS_TEMP="~/tmp/emacs-26.2.tar.gz"
 curl https://mirrors.ocf.berkeley.edu/gnu/emacs/emacs-26.2.tar.gz -o "$EMACS_TEMP"
 tar -xvzf "$EMACS_TEMP" -C "$EMACS_TEMP_FOLDER"
-$EMACS_TEMP_FOLDER/emacs-26.2/configure
-$EMACS_TEMP_FOLDERm/emacs-26.2/make
-sudo $EMACS_TEMP_FOLDER/emacs-26.2/make install
+cd $EMACS_TEMP_FOLDER/emacs-26.2/
+./configure
+./make
+sudo $./make install
+cd -
