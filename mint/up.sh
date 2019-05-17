706a5340cd8b8fce0ab9848dde934263af2ddcce
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-17 15:48:55 +0100

    Deleted libraries that are loaded through build-dep

--- mint/up.sh
+++ mint/up.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 # Basic packages
-sudo apt-get -y install zsh curl wget net-tools software-properties-common coreutils xclip libssl-dev libreadline-dev zlib1g-dev mercurial kdiff3 firefox unzip zip libgnutls30 libgnutls28-dev
+sudo apt-get -y install zsh curl wget net-tools software-properties-common coreutils xclip libssl-dev libreadline-dev zlib1g-dev mercurial kdiff3 firefox unzip zip
 
 # Install chrome
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/chrome.list
