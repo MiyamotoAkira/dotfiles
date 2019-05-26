65d1ec19b28218ad78d1837fdce53f9510a770de
MiyamotoAkira <yefo.akira@gmail.com>
2019-05-26 09:03:05 +0100

    Added mono installation

--- mint/up.sh
+++ mint/up.sh
@@ -28,6 +28,7 @@ sudo apt-get -y install \
      jq \
      apt-transport-https \
      ca-certificates \
+     gnupg \
      gnupg-agent \
      python-requests
 
@@ -44,6 +45,12 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get -y install apt-transport-https
 sudo apt-get -y update
 sudo apt-get -y install dotnet-sdk-2.2
+
+# Mono
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt-get -y install mono-complete
 
 # preparing for building emacs
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'No configuration'"
