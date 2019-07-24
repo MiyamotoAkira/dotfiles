fc113fb4cd454c65ff1214f46f5e9115d6c44f0d
MiyamotoAkira <yefo.akira@gmail.com>
2019-07-24 15:39:50 +0100

    Fixing mint erlang install

--- mint/up.sh
+++ mint/up.sh
@@ -53,7 +53,7 @@ sudo apt update
 sudo apt-get -y install mono-complete
 
 # Elixir
-sudo get https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+sudo wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
 sudo apt-get install esl-erlang
 sudo apt-get install elixir
