55d2298428f2576e47da14223d1a2d8f6dc4135b
MiyamotoAkira <yefo.akira@gmail.com>
2019-08-03 10:15:29 +0100

    Trying to fix mono install

--- fedora/up.sh
+++ fedora/up.sh
@@ -44,7 +44,7 @@ sudo dnf -y install dotnet-sdk-2.2
 
 # Install Mono
 sudo rpm --import "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-sudo curl https://download.mono-project.com/repo/centos8-stable.repo | tee /etc/yum.repos.d/mono-centos8-stable.repo
+sudo sh -c "curl https://download.mono-project.com/repo/centos8-stable.repo | tee /etc/yum.repos.d/mono-centos8-stable.repo"
 sudo dnf -y update
 sudo dnf -y install mono-complete
 
