54215e85d97688fac95ddc6ef3c4d43d6dca036b
MiyamotoAkira <yefo.akira@gmail.com>
2020-05-30 16:29:45 +0100

    Start the move into ansible

--- fedora/up.sh
+++ fedora/up.sh
@@ -2,72 +2,76 @@
 
 set -euo pipefail
 
-sudo dnf -y groupinstall "C Development Tools and Libraries"
-sudo dnf -y groupinstall "Development Tools"
+sudo dnf updgrade
+sudo dnf install ansible
 
-sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+ansible ../ansible/full.yml
+# sudo dnf -y groupinstall "C Development Tools and Libraries"
+# sudo dnf -y groupinstall "Development Tools"
 
-sudo dnf -y groupupdate core
-sudo dnf -y --best --allowerasing install \
-     zsh \
-     curl \
-     wget \
-     util-linux-user \
-     mercurial \
-     kdiff3 \
-     openssl-devel \
-     readline-devel \
-     ffmpeg-libs \
-     dnf-plugins-core \
-     firefox \
-     unzip \
-     zip \
-     pandoc \
-     the_silver_searcher \
-     vim-enhanced \
-     gnome-tweak-tool \
-     elixir\
-     bat \
-     exa \
-     jq
+# sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-# Install chrome
-sudo dnf -y install fedora-workstation-repositories
-sudo dnf -y config-manager --set-enabled google-chrome
+# sudo dnf -y groupupdate core
+# sudo dnf -y --best --allowerasing install \
+#      zsh \
+#      curl \
+#      wget \
+#      util-linux-user \
+#      mercurial \
+#      kdiff3 \
+#      openssl-devel \
+#      readline-devel \
+#      ffmpeg-libs \
+#      dnf-plugins-core \
+#      firefox \
+#      unzip \
+#      zip \
+#      pandoc \
+#      the_silver_searcher \
+#      vim-enhanced \
+#      gnome-tweak-tool \
+#      elixir\
+#      bat \
+#      exa \
+#      jq
 
-# Dotnet packages
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-wget -q https://packages.microsoft.com/config/fedora/30/prod.repo
-sudo mv prod.repo /etc/yum.repos.d/microsoft-prod.repo
-sudo chown root:root /etc/yum.repos.d/microsoft-prod.repo
+# # Install chrome
+# sudo dnf -y install fedora-workstation-repositories
+# sudo dnf -y config-manager --set-enabled google-chrome
 
-sudo dnf -y update
-sudo dnf -y install dotnet-sdk-3.0
+# # Dotnet packages
+# sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# wget -q https://packages.microsoft.com/config/fedora/30/prod.repo
+# sudo mv prod.repo /etc/yum.repos.d/microsoft-prod.repo
+# sudo chown root:root /etc/yum.repos.d/microsoft-prod.repo
 
-# Install Mono
-sudo rpm --import "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-sudo sh -c "curl https://download.mono-project.com/repo/centos8-stable.repo | tee /etc/yum.repos.d/mono-centos8-stable.repo"
-sudo dnf -y update
-sudo dnf -y install mono-complete
+# sudo dnf -y update
+# sudo dnf -y install dotnet-sdk-3.0
 
-# Preparation for Emacs
-sudo dnf -y builddep emacs
+# # Install Mono
+# sudo rpm --import "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
+# sudo sh -c "curl https://download.mono-project.com/repo/centos8-stable.repo | tee /etc/yum.repos.d/mono-centos8-stable.repo"
+# sudo dnf -y update
+# sudo dnf -y install mono-complete
 
-# Install dropbox
-curl -O https://linux.dropbox.com/packages/fedora/nautilus-dropbox-2019.02.14-1.fedora.x86_64.rpm
-sudo dnf -y install nautilus-dropbox-2019.02.14-1.fedora.x86_64.rpm
+# # Preparation for Emacs
+# sudo dnf -y builddep emacs
 
-# installing docker
-# sudo dnf config-manager \
-#      --add-repo \
-#      https://download.docker.com/linux/fedora/docker-ce.repo
-# sudo dnf -y install docker-ce docker-ce-cli containerd.io
-# sudo usermod -aG docker $(whoami)
-# sudo systemctl start docker
-# sudo systemctl enable docker
+# # Install dropbox
+# curl -O https://linux.dropbox.com/packages/fedora/nautilus-dropbox-2019.02.14-1.fedora.x86_64.rpm
+# sudo dnf -y install nautilus-dropbox-2019.02.14-1.fedora.x86_64.rpm
 
-# install slack
-curl -O https://downloads.slack-edge.com/linux_releases/slack-3.4.2-0.1.fc21.x86_64.rpm
-sudo dnf -y install slack-3.4.2-0.1.fc21.x86_64.rpm
+# # installing docker
+# # sudo dnf config-manager \
+# #      --add-repo \
+# #      https://download.docker.com/linux/fedora/docker-ce.repo
+# # sudo dnf -y install docker-ce docker-ce-cli containerd.io
+# # sudo usermod -aG docker $(whoami)
+# # sudo systemctl start docker
+# # sudo systemctl enable docker
 
-"common/common-configuration.sh"
+# # install slack
+# curl -O https://downloads.slack-edge.com/linux_releases/slack-3.4.2-0.1.fc21.x86_64.rpm
+# sudo dnf -y install slack-3.4.2-0.1.fc21.x86_64.rpm
+
+# "common/common-configuration.sh"
--- mint/up.sh
+++ mint/up.sh
@@ -5,100 +5,105 @@ set -euo pipefail
 sudo apt-get update
 sudo apt-get -y upgrade
 
-# Basic packages
-sudo apt-get -y install \
-     zsh \
-     curl \
-     wget \
-     net-tools \
-     software-properties-common \
-     coreutils \
-     xclip \
-     libssl-dev \
-     libreadline-dev \
-     zlib1g-dev \
-     mercurial \
-     kdiff3 \
-     firefox \
-     unzip \
-     zip \
-     pandoc \
-     silversearcher-ag \
-     devscripts \
-     jq \
-     apt-transport-https \
-     ca-certificates \
-     gnupg \
-     gnupg-agent \
-     python-requests
+sudo apt install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
 
-# Install chrome
-echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
-sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-sudo apt-get -y update
-sudo apt-get -y install google-chrome-stable
+ansible ../ansible/full.yml
+# # Basic packages
+# sudo apt-get -y install \
+#      zsh \
+#      curl \
+#      wget \
+#      net-tools \
+#      software-properties-common \
+#      coreutils \
+#      xclip \
+#      libssl-dev \
+#      libreadline-dev \
+#      zlib1g-dev \
+#      mercurial \
+#      kdiff3 \
+#      firefox \
+#      unzip \
+#      zip \
+#      pandoc \
+#      silversearcher-ag \
+#      devscripts \
+#      jq \
+#      apt-transport-https \
+#      ca-certificates \
+#      gnupg \
+#      gnupg-agent \
+#      python-requests
 
-# Install bat
-wget -q https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb
-sudo dpkg -i bat_0.12.1_amd64.deb
-rm bat_0.12.1_amd64.deb
+# # Install chrome
+# echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+# sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+# sudo apt-get -y update
+# sudo apt-get -y install google-chrome-stable
 
-# Install exa
-wget -q https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
-unzip exa-linux-x86_64-0.9.0.zip
-mkdir ~/bin
-mv exa-linux-x86_64 ~/bin/exa
-rm exa-linux-x86_64-0.9.0.zip
+# # Install bat
+# wget -q https://github.com/sharkdp/bat/releases/download/v0.12.1/bat_0.12.1_amd64.deb
+# sudo dpkg -i bat_0.12.1_amd64.deb
+# rm bat_0.12.1_amd64.deb
 
-# Dot net packages
-wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
+# # Install exa
+# wget -q https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
+# unzip exa-linux-x86_64-0.9.0.zip
+# mkdir ~/bin
+# mv exa-linux-x86_64 ~/bin/exa
+# rm exa-linux-x86_64-0.9.0.zip
 
-sudo add-apt-repository -y universe
-sudo apt-get -y update
-sudo apt-get -y install apt-transport-https
-sudo apt-get -y update
-sudo apt-get -y install dotnet-sdk-3.0
+# # Dot net packages
+# wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+# sudo dpkg -i packages-microsoft-prod.deb
 
-# Mono
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-sudo apt update
-sudo apt-get -y install mono-complete
+# sudo add-apt-repository -y universe
+# sudo apt-get -y update
+# sudo apt-get -y install apt-transport-https
+# sudo apt-get -y update
+# sudo apt-get -y install dotnet-sdk-3.0
 
-# Elixir
-# sudo wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
-# sudo apt-get update
-# sudo apt-get install esl-erlang
-# sudo apt-get install elixir
+# # Mono
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+# echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+# sudo apt update
+# sudo apt-get -y install mono-complete
 
-# preparing for building emacs
-debconf-set-selections <<< "postfix postfix/main_mailer_type string 'No configuration'"
-debconf-set-selections <<< "postfix postfix/mailname string twoormore.eu"
-sudo apt-get -y build-dep emacs25
+# # Elixir
+# # sudo wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+# # sudo apt-get update
+# # sudo apt-get install esl-erlang
+# # sudo apt-get install elixir
 
-#Install dropbox
-# curl -O https://linux.dropbox.com/packages/ubuntu/dropbox_2019.02.14_amd64.deb
-# sudo mk-build-deps -i dropbox_2019.02.14_amd64.deb
-# sudo apt-get -y install libpango1.0-0
-# sudo dpkg -i dropbox_2019.02.14_amd64.deb
-sudo apt-get -y install nautilus-dropbox
+# # preparing for building emacs
+# debconf-set-selections <<< "postfix postfix/main_mailer_type string 'No configuration'"
+# debconf-set-selections <<< "postfix postfix/mailname string twoormore.eu"
+# sudo apt-get -y build-dep emacs25
+
+# #Install dropbox
+# # curl -O https://linux.dropbox.com/packages/ubuntu/dropbox_2019.02.14_amd64.deb
+# # sudo mk-build-deps -i dropbox_2019.02.14_amd64.deb
+# # sudo apt-get -y install libpango1.0-0
+# # sudo dpkg -i dropbox_2019.02.14_amd64.deb
+# sudo apt-get -y install nautilus-dropbox
 
 
-# installing docker
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo add-apt-repository \
-#    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#    $(lsb_release -cs) \
-#    stable"
-# sudo apt-get update
-# sudo apt-get install docker-ce docker-ce-cli containerd.io
-# sudo usermod -aG docker $(whoami)
-# sudo systemctl start docker
-# sudo systemctl enable docker
+# # installing docker
+# # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# # sudo add-apt-repository \
+# #    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+# #    $(lsb_release -cs) \
+# #    stable"
+# # sudo apt-get update
+# # sudo apt-get install docker-ce docker-ce-cli containerd.io
+# # sudo usermod -aG docker $(whoami)
+# # sudo systemctl start docker
+# # sudo systemctl enable docker
 
-sudo apt-get -y install gconf2 gconf-service libnotify4 gvfs-bin libappindicator1 libsecret-1-0
-curl -O https://downloads.slack-edge.com/linux_releases/slack-desktop-3.4.2-amd64.deb
-sudo dpkg -i slack-desktop-3.4.2-amd64.deb
+# sudo apt-get -y install gconf2 gconf-service libnotify4 gvfs-bin libappindicator1 libsecret-1-0
+# curl -O https://downloads.slack-edge.com/linux_releases/slack-desktop-3.4.2-amd64.deb
+# sudo dpkg -i slack-desktop-3.4.2-amd64.deb
 
-"common/common-configuration.sh"
+# "common/common-configuration.sh"
