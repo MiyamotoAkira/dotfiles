a826da5cb303bd8a23d8085e20659489174e125c
MiyamotoAkira <yefo.akira@gmail.com>
2020-07-24 06:57:39 +0100

    Moved to Linux Mint 20

--- mint/up.sh
+++ mint/up.sh
@@ -21,7 +21,7 @@ ansible-playbook ansible/full.yml
 
 # # Mono
 # sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-# echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+# echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 # sudo apt update
 # sudo apt-get -y install mono-complete
 
