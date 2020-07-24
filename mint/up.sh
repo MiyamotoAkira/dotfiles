9adeb58dd3674d111dd5ed108045c658dd9f9648
MiyamotoAkira <yefo.akira@gmail.com>
2020-07-24 07:14:26 +0100

    Trying the update first on Mint

--- mint/up.sh
+++ mint/up.sh
@@ -2,8 +2,8 @@
 
 set -euo pipefail
 
-sudo apt-get -y install python3-requests
 sudo apt-get update
+sudo apt-get -y install python3-requests
 sudo apt-get -y upgrade
 
 sudo apt-add-repository --yes ppa:ansible/ansible
