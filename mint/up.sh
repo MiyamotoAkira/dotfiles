8adc47b0963080073112283f36627a8f3b010904
MiyamotoAkira <yefo.akira@gmail.com>
2020-12-12 13:17:46 +0000

    Adding docker to installation on Mint

--- mint/up.sh
+++ mint/up.sh
@@ -34,13 +34,6 @@ ansible-playbook ansible/full.yml
 # sudo apt-get -y build-dep emacs25
 
 # # installing docker
-# # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# # sudo add-apt-repository \
-# #    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-# #    $(lsb_release -cs) \
-# #    stable"
-# # sudo apt-get update
-# # sudo apt-get install docker-ce docker-ce-cli containerd.io
 # # sudo usermod -aG docker $(whoami)
 # # sudo systemctl start docker
 # # sudo systemctl enable docker
