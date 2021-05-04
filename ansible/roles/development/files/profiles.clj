c597fbb257cb02bbc1a21c7b8f2542df38114cee
MiyamotoAkira <yefo.akira@twoormore.eu>
2021-05-04 06:05:25 +0100

    Updated versions for the lein profile

--- ansible/roles/development/files/profiles.clj
+++ ansible/roles/development/files/profiles.clj
@@ -1,9 +1,9 @@
 {:user
  {;; :dependencies [[nrepl "0.5.3"]]
-  :plugins [[lein-pprint "1.2.0"]
-            [lein-cprint "1.3.0"]
-            [com.jakemccrary/lein-test-refresh "0.23.0"]
-            [lein-cljfmt "0.6.3"]]}
+  :plugins [[lein-pprint "1.3.2"]
+            [lein-cprint "1.3.3"]
+            [com.jakemccrary/lein-test-refresh "0.24.1"]
+            [lein-cljfmt "0.7.0"]]}
  ;; {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]
  ;;            [venantius/ultra "0.5.2"]
  ;;            [lein-pprint "1.2.0"]
@@ -12,4 +12,4 @@
  ;;            [mvxcvi/whidbey "1.3.2"]
  ;;            [nrepl/lein-nrepl "0.3.1"]]}
  }
-{:repl {:plugins [[cider/cider-nrepl "0.18.0"]]}}
+{:repl {:plugins [[cider/cider-nrepl "0.25.9"]]}}
