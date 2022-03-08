7399771f6873ac85b6091cbd49f60facdb66fb37
MiyamotoAkira <yefo.akira@twoormore.eu>
2022-03-08 14:52:07 +0000

    Updated some of my dependencies

--- ansible/roles/development/files/profiles.clj
+++ ansible/roles/development/files/profiles.clj
@@ -2,7 +2,7 @@
  {;; :dependencies [[nrepl "0.5.3"]]
   :plugins [[lein-pprint "1.3.2"]
             [lein-cprint "1.3.3"]
-            [com.jakemccrary/lein-test-refresh "0.24.1"]
+            [com.jakemccrary/lein-test-refresh "0.25.0"]
             [lein-cljfmt "0.8.0"]]}
  ;; {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]
  ;;            [venantius/ultra "0.5.2"]
@@ -12,4 +12,4 @@
  ;;            [mvxcvi/whidbey "1.3.2"]
  ;;            [nrepl/lein-nrepl "0.3.1"]]}
  }
-{:repl {:plugins [[cider/cider-nrepl "0.26.0"]]}}
+{:repl {:plugins [[cider/cider-nrepl "0.28.3"]]}}
